355acd92c79b3a03fc6b8a3bad7ee29d37da3193
mwb <mbarbine@boxzor.com>
2020-04-04 20:58:33 +0000

    updated email verificaton"

--- controllers/user.js
+++ controllers/user.js
@@ -527,8 +527,8 @@ exports.postForgot = (req, res, next) => {
     });
     const mailOptions = {
       to: user.email,
-      from: 'hackathon@starter.com',
-      subject: 'Reset your password on Hackathon Starter',
+      from: 'NoReply@PH3AR.com',
+      subject: 'Reset your password at PH3AR@Virtual',
       text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
         http://${req.headers.host}/reset/${token}\n\n
